f794bb4bdb383a7928c13dda675e2f8809d931fb
Gleb Batykov <gleb.batykov@gmail.com>
2022-07-22 01:04:58 +0300

    Updates example

--- example/hello_world.dart
+++ example/hello_world.dart
@@ -1,14 +1,18 @@
 import 'package:minerva/minerva.dart';
 
 void endpointsBuilder(Endpoints endpoints) {
-  //
-  endpoints.get('/hello', (context, request) => 'Hello, world!');
+  // Create route for GET requests with path '/hello'
+  endpoints.get('/hello', (context, request) {
+    var message = context.store['message'];
+
+    return message;
+  });
 }
 
 void main() async {
-  //
-  var setting = ServerSetting('127.0.0.1', 5555);
+  // Create server setting
+  var setting = ServerSetting('127.0.0.1', 5000);
 
-  //
+  // Bind server
   await Minerva.bind(setting: setting, builder: endpointsBuilder);
 }
