17e010fac751575a0de74dcf6c01439aaeb273ff
Gleb Batykov <gleb.batykov@gmail.com>
2022-07-26 19:52:28 +0300

    Adds core functionality

--- lib/src/core.dart
+++ lib/src/core.dart
@@ -7,3 +7,5 @@ import 'package:path/path.dart';
 part 'core/minerva_exception.dart';
 
 part 'core/extension/file_extension.dart';
+
+part 'core/project/project.dart';
---
+++ lib/src/core/project/project.dart
@@ -0,0 +1,19 @@
+part of minerva_core;
+
+abstract class Project {
+  static String? _projectPath;
+
+  static String get projectPath {
+    if (_projectPath == null) {
+      var scriptPath = Uri.directory(Platform.script.path);
+
+      _projectPath = scriptPath.pathSegments
+          .getRange(0, scriptPath.pathSegments.length - 3)
+          .join('/');
+
+      print(_projectPath);
+    }
+
+    return _projectPath!;
+  }
+}
