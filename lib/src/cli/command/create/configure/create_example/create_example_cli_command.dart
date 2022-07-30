4c3a75463262ad8fe03d743c55abb32cf7e1bb2d
Gleb Batykov <gleb.batykov@gmail.com>
2022-07-30 22:45:16 +0300

    Updates application tamplate

--- lib/src/cli/command/create/configure/create_example/create_example_cli_command.dart
+++ lib/src/cli/command/create/configure/create_example/create_example_cli_command.dart
@@ -9,59 +9,153 @@ class CreateExampleCLICommand extends CLICommand<void> {
   Future<void> run() async {
     var mainFile = File.fromUri(Uri.file('$projectPath/lib/main.dart'));
 
-    var endpointBuilderFile =
-        File.fromUri(Uri.file('$projectPath/lib/endpoints_builder.dart'));
+    var endpointBuilderFile = File.fromUri(
+        Uri.file('$projectPath/lib/builder/endpoints_builder.dart'));
 
     var serverBuilderFile =
-        File.fromUri(Uri.file('$projectPath/lib/server_builder.dart'));
+        File.fromUri(Uri.file('$projectPath/lib/builder/server_builder.dart'));
+
+    var apisBuilderFile =
+        File.fromUri(Uri.file('$projectPath/lib/builder/apis_builder.dart'));
+
+    var loggersBuilderFile =
+        File.fromUri(Uri.file('$projectPath/lib/builder/loggers_builder.dart'));
+
+    var settingBuilderFile =
+        File.fromUri(Uri.file('$projectPath/lib/builder/setting_builder.dart'));
+
+    var middlewaresBuilderFile = File.fromUri(
+        Uri.file('$projectPath/lib/builder/middleware_builder.dart'));
 
     await Future.wait([
-      mainFile.create(),
-      endpointBuilderFile.create(),
-      serverBuilderFile.create()
+      mainFile.create(recursive: true),
+      endpointBuilderFile.create(recursive: true),
+      serverBuilderFile.create(recursive: true),
+      apisBuilderFile.create(recursive: true),
+      loggersBuilderFile.create(recursive: true),
+      settingBuilderFile.create(recursive: true),
+      middlewaresBuilderFile.create(recursive: true)
     ]);
 
-    var endpointBuilderContent = '''
+    var endpointsBuilderContent = '''
 import 'package:minerva/minerva.dart';
 
-void endpointsBuilder(Endpoints endpoints) {
-  // Create route for GET requests with path '/hello'
-  endpoints.get('/hello', (context, request) {
-    var message = context.store['message'];
+class EndpointsBuilder extends MinervaEndpointsBuilder {
+  @override
+  void build(Endpoints endpoints) {
+    // Create route for GET requests with path '/hello'
+    endpoints.get('/hello', (context, request) {
+      var message = context.store['message'];
 
-    return message;
-  });
+      return message;
+    });
+  }
 }
 ''';
 
     var serverBuilderContent = '''
 import 'package:minerva/minerva.dart';
 
-void serverBuilder(ServerContext context) {
-  // Inject dependency or resource
-  context.store['message'] = 'Hello, world!';
+class ServerBuilder extends MinervaServerBuilder {
+  @override
+  void build(ServerContext context) {
+    // Inject dependency or resource
+    context.store['message'] = 'Hello, world!';
+  }
 }
 ''';
 
     var mainContent = '''
 import 'package:minerva/minerva.dart';
 
-import 'endpoints_builder.dart';
-import 'server_builder.dart';
+import 'builder/setting_builder.dart';
 
 void main(List<String> args) async {
-  // Create server setting
-  var setting = MinervaSetting(endpointsBuilder: endpointsBuilder, serverBuilder: serverBuilder);
-
   // Bind server
-  await Minerva.bind(args: args, setting: setting);
+  await Minerva.bind(args: args, setting: SettingBuilder().build());
+}
+''';
+
+    var apisBuilderContent = '''
+import 'package:minerva/minerva.dart';
+
+class ApisBuilder extends MinervaApisBuilder {
+  @override
+  List<Api> build() {
+    var apis = <Api>[];
+
+    return apis;
+  }
+}
+''';
+
+    var loggersBuilderContent = '''
+import 'package:minerva/minerva.dart';
+
+class LoggersBuilder extends MinervaLoggersBuilder {
+  @override
+  List<Logger> build() {
+    var loggers = <Logger>[];
+
+    //
+    loggers.add(MinervaLogger());
+
+    return loggers;
+  }
+}
+''';
+
+    var middlewaresBuilderContent = '''
+import 'package:minerva/minerva.dart';
+
+class MiddlewaresBuilder extends MinervaMiddlewaresBuilder {
+  @override
+  List<Middleware> build() {
+    var middlewares = <Middleware>[];
+
+    //
+    middlewares.add(ErrorMiddleware());
+
+    //
+    middlewares.add(EndpointMiddleware());
+
+    return middlewares;
+  }
+}
+''';
+
+    var settingBuilderContent = '''
+import 'package:minerva/minerva.dart';
+
+import 'apis_builder.dart';
+import 'endpoints_builder.dart';
+import 'middleware_builder.dart';
+import 'server_builder.dart';
+import 'loggers_builder.dart';
+
+class SettingBuilder extends MinervaSettingBuilder {
+  @override
+  MinervaSetting build() {
+    //
+    return MinervaSetting(
+        instance: 12,
+        loggersBuilder: LoggersBuilder(),
+        endpointsBuilder: EndpointsBuilder(),
+        serverBuilder: ServerBuilder(),
+        apisBuilder: ApisBuilder(),
+        middlewaresBuilder: MiddlewaresBuilder());
+  }
 }
 ''';
 
     await Future.wait([
-      endpointBuilderFile.writeAsString(endpointBuilderContent),
+      endpointBuilderFile.writeAsString(endpointsBuilderContent),
       serverBuilderFile.writeAsString(serverBuilderContent),
-      mainFile.writeAsString(mainContent)
+      mainFile.writeAsString(mainContent),
+      apisBuilderFile.writeAsString(apisBuilderContent),
+      loggersBuilderFile.writeAsString(loggersBuilderContent),
+      settingBuilderFile.writeAsString(settingBuilderContent),
+      middlewaresBuilderFile.writeAsString(middlewaresBuilderContent)
     ]);
   }
 }
