e623030f345646571986d759c72c7fb2195824b4
Gleb Batykov <gleb.batykov@gmail.com>
2022-08-02 00:12:40 +0300

    Updates RequestBody

--- lib/src/http/request_body.dart
+++ lib/src/http/request_body.dart
@@ -1,22 +1,31 @@
 part of minerva_http;
 
 class RequestBody {
-  final Stream<Uint8List> _data;
+  final Stream<Uint8List> _dataStream;
+
+  List<Uint8List>? _data;
 
   String? _text;
 
   Map<String, dynamic>? _json;
 
-  RequestBody(Stream<Uint8List> data) : _data = data;
+  RequestBody(Stream<Uint8List> dataStream) : _dataStream = dataStream;
+
+  Future<List<Uint8List>> get data async {
+    _data ??= await _dataStream.toList();
+
+    return _data!;
+  }
 
   Future<String> asText() async {
-    _text ??= await utf8.decodeStream(_data);
+    _text ??= await utf8.decodeStream(Stream.fromIterable(await data));
 
     return _text!;
   }
 
   Future<Map<String, dynamic>> asJson() async {
-    _json ??= jsonDecode(await utf8.decodeStream(_data));
+    _json ??=
+        jsonDecode(await utf8.decodeStream(Stream.fromIterable(await data)));
 
     return _json!;
   }
