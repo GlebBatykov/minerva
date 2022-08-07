c08837dd055dfa67f161f51e39c06f2974bb9a78
Gleb Batykov <gleb.batykov@gmail.com>
2022-08-07 15:47:56 +0300

    Updates FileLoggerAgentData class

--- lib/src/logging/file/file_logger_agent_data.dart
+++ lib/src/logging/file/file_logger_agent_data.dart
@@ -1,6 +1,7 @@
 part of minerva_logging;
 
 class FileLoggerAgentData extends AgentData {
-  FileLoggerAgentData({String name = 'file_logger'})
-      : super(name, FileLoggerAgent());
+  FileLoggerAgentData(
+      {String name = 'file_logger', String logPath = '~/log/log.log'})
+      : super(name, FileLoggerAgent(), data: {'logPath': logPath});
 }
